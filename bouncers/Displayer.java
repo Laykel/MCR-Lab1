76ac40a69f24da3854e69eee777d14116ba3202b
Laykel <luc.wachter@gmail.com>
2019-03-04 22:00:16 +0100

    Added Displayer interface

---
+++ bouncers/Displayer.java
@@ -0,0 +1,12 @@
+package bouncers;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+public interface Displayer {
+    int getWidth();
+    int getHeight();
+    Graphics2D getGraphics();
+    void repaint();
+    void setTitle(String s);
+}
